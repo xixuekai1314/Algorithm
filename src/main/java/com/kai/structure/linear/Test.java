51fe4dfd11f4c24b40b85500356ff1b3ac16e486
南苹果 <845676529@qq.com>
2023-07-19 14:33:37 +0800

    LinkedList

--- src/main/java/com/kai/structure/linear/Test.java
+++ src/main/java/com/kai/structure/linear/Test.java
@@ -1,6 +1,6 @@
 package com.kai.structure.linear;
 
-import com.kai.structure.linear.list.ArrayList;
+import com.kai.structure.linear.list.LinkedList;
 
 /**
  * @ClassName: Test
@@ -12,7 +12,7 @@ import com.kai.structure.linear.list.ArrayList;
 public class Test {
 
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<String>();
+        LinkedList list = new LinkedList();
         System.out.println(list.isEmpty());
         for (int i = 0; i < 12; i++) {
             list.add(i + "");
@@ -20,12 +20,15 @@ public class Test {
         System.out.println(list.toString());
         System.out.println(list.isEmpty());
         list.add(0,"0位置添加");
-        list.addAfter("0","after");
+        System.out.println(list.toString());
         list.addBefore("0","before");
+        System.out.println(list.toString());
+        list.addAfter("0","after");
         System.out.println(list.toString());
         System.out.println(list.contains("0"));
         System.out.println(list.get(0));
         list.set(0,"0位置修改");
+        System.out.println(list.contains("0"));
         System.out.println(list.toString());
         list.remove("0");
         System.out.println(list.toString());
@@ -33,9 +36,6 @@ public class Test {
         System.out.println(list.toString());
         list.clear();
         System.out.println(list.toString());
-        Object[] elementData = list.getElementData();
         System.out.println(list.size());
-        System.out.println(elementData.length);
-
     }
 }
--- src/main/java/com/kai/structure/linear/list/LinkedList.java
+++ src/main/java/com/kai/structure/linear/list/LinkedList.java
@@ -1,6 +1,7 @@
 package com.kai.structure.linear.list;
 
 import com.kai.structure.linear.List;
+import org.springframework.boot.context.properties.bind.Nested;
 
 /**
  * @ClassName: LinkedList
@@ -48,50 +49,83 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) >= 0;
     }
 
+    /**
+     * @Description: 目前猜测这里区分空和equals还是为了减少一次调用
+     * 虽然这么写会造成代码的冗余，但是减少了equals再调用null判断的这个过程
+     * @Author: Kai
+     * @Date: 2023/7/13 17:22
+     */
     @Override
     public int indexOf(Object o) {
-        return 0;
+        int index = 0;
+        if (o == null) {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (x.item == null) return index;
+                index++;
+            }
+        } else {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (o.equals(x.item)) return index;
+                index++;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int add(E e) {
-        return 0;
+        linkLast(e);
+        return size;
     }
 
     @Override
     public void add(int index, E element) {
-
+        isPositionIndex(index);
+        if (index == size) linkLast(element);
+        else linkBefore(element, node(index));
     }
 
     @Override
     public int addBefore(Object obj, E e) {
-        return 0;
+        /**
+         * @Description: 其实这里这个这样可以实现功能   linkBefore(e, Node(obj));
+         *                  但是这里要返回int，就先用两次循环吧，后续有时间可以优化一下
+         * @Author: Kai
+         * @Date: 2023/7/18 17:11
+         */
+        int x = indexOf(obj);
+        if (x == -1) throw new IllegalArgumentException("参数不存在");
+        linkBefore(e, node(x));
+        return x;
     }
 
     @Override
     public int addAfter(Object obj, E e) {
-        return 0;
+        int x = indexOf(obj);
+        if (x == -1) throw new IllegalArgumentException("参数不存在");
+        linkAfter(e, node(x));
+        return x;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        unlink(node(o));
+        return true;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        checkElementIndex(index);
+        return unlink(node(index));
     }
 
     @Override
     public void clear() {
-
+        unlinkAll();
     }
-
-
 
 
     /**
@@ -102,7 +136,7 @@ public class LinkedList<E> implements List<E> {
      * @Author: Kai
      * @Date: 2023/7/7 08:18
      */
-    private static class Node<E> {
+    public static class Node<E> {
         E item;
         Node<E> next;
         Node<E> prev;
@@ -116,14 +150,15 @@ public class LinkedList<E> implements List<E> {
 
     /**
      * @Description: 判断index的正确性
-     *                  通过查找源码发现这里其实还有一处调用，在获取node的时候，只是被注释掉了
-     *                  所以这里是判断正确的，校验方法要再套一层
+     * 通过查找源码发现这里其实还有一处调用，在获取node的时候，只是被注释掉了
+     * 所以这里是判断正确的，校验方法要再套一层
      * @Author: Kai
      * @Date: 2023/7/12 15:03
      */
     private boolean isElementIndex(int index) {
         return index >= 0 && index < size;
     }
+
     /**
      * @Description: 校验index的正确性，并抛出异常
      * @Author: Kai
@@ -131,6 +166,27 @@ public class LinkedList<E> implements List<E> {
      */
     private void checkElementIndex(int index) {
         if (!isElementIndex(index))
+            throw new IndexOutOfBoundsException("index小于0或大于等于size");
+    }
+
+    /**
+     * @Description: 判断index的是否超出约束条件（此处的约束条件不是正确性，范围比正确性大 1）
+     * 正常的index应该是小于size的，但是这里给迭代器和添加使用了
+     * 因为正常是size-1个元素，但是第size-1个元素有next节点（null），也就是可以视为第size个元素
+     * @Author: Kai
+     * @Date: 2023/7/18 14:16
+     */
+    private boolean isPositionIndex(int index) {
+        return index >= 0 && index <= size;
+    }
+
+    /**
+     * @Description: 校验index的是否超出约束条件，并抛出异常
+     * @Author: Kai
+     * @Date: 2023/7/18 15:06
+     */
+    private void checkPositionIndex(int index) {
+        if (!isPositionIndex(index))
             throw new IndexOutOfBoundsException("index小于0或大于size");
     }
 
@@ -144,6 +200,7 @@ public class LinkedList<E> implements List<E> {
          * @Description: 源码中这里的校验被注释掉了，应该是最开始写在了获取的地方
          *                  后续增加了其他方法之后，不适合在较为靠后的位置校验（比如addall方法）
          *                  将所有校验提前，而注释了这里
+         *                  index是从 0 开始的，也就是node（3） 获取的是第四个元素
          *                  // assert isElementIndex(index);
          * @Author: Kai
          * @Date: 2023/7/12 15:11
@@ -163,5 +220,156 @@ public class LinkedList<E> implements List<E> {
                 x = x.prev;
             return x;
         }
+    }
+
+    /**
+     * @Description: 获取o所在节点
+     * 这个只能顺查，没有办法提前预知o位于前半段还是后半段
+     * @Author: Kai
+     * @Date: 2023/7/18 17:03
+     */
+    public Node<E> node(Object o) {
+        if (o == null) {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (x.item == null)
+                    return x;
+            }
+        } else {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (o.equals(x.item))
+                    return x;
+            }
+        }
+        throw new IllegalArgumentException("参数不存在");
+    }
+
+    /**
+     * @Description: 在头部新增
+     * @Author: Kai
+     * @Date: 2023/7/18 14:07
+     */
+    private void linkFirst(E e) {
+        final Node<E> f = first;
+        final Node<E> newNode = new Node<>(null, e, f);
+        first = newNode;
+        if (f == null)
+            last = newNode;
+        else
+            f.prev = newNode;
+        size++;
+    }
+
+    /**
+     * @Description: 在尾部新增
+     * @Author: Kai
+     * @Date: 2023/7/18 13:56
+     */
+    void linkLast(E e) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
+    /**
+     * @Description: 在succ前新增，是因为调用这里使用的是 node（index）
+     * 这里的常规理解是 index 从 1 开始的
+     * 而正常 开发习惯 index 从 0 开始的
+     * 所以这里是在 succ前新增
+     * @Author: Kai
+     * @Date: 2023/7/18 15:09
+     */
+    void linkBefore(E e, Node<E> succ) {
+        final Node<E> pred = succ.prev;
+        final Node<E> newNode = new Node<>(pred, e, succ);
+        succ.prev = newNode;
+        if (pred == null) first = newNode;
+        else pred.next = newNode;
+        size++;
+    }
+
+    /**
+     * @Description: 在succ后新增
+     * 这个方法不是linkedList里的方法
+     * @Author: Kai
+     * @Date: 2023/7/18 15:09
+     */
+    void linkAfter(E e, Node<E> succ) {
+        final Node<E> next = succ.next;
+        final Node<E> newNode = new Node<>(succ, e, next);
+        succ.next = newNode;
+        //前节点的next 指向新节点
+        if (next == null) last = newNode;
+        else next.prev = newNode;
+        size++;
+    }
+
+    /**
+     * @Description: 清除节点x
+     * 这里要将节点x的前节点，后节点，内容全部清除
+     * 源代码是这么解释的：清除不是必须的，但是有助于垃圾回收并且有迭代器可以关联到也会释放内存
+     * Clearing all of the links between nodes is "unnecessary"
+     * but
+     * helps a generational GC if the discarded nodes inhabit more than one generation
+     * is sure to free memory even if there is a reachable Iterator
+     * @Author: Kai
+     * @Date: 2023/7/18 17:49
+     */
+    E unlink(Node<E> x) {
+        final E element = x.item;
+        final Node<E> next = x.next;
+        final Node<E> prev = x.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size--;
+        return element;
+    }
+
+    /**
+     * @Description: 这里其实多一层跳转会增加寻址的消耗
+     * 但是放在这里可以方便关联上边关于 全部设置成 null 的解释
+     * @Author: Kai
+     * @Date: 2023/7/18 17:54
+     */
+    void unlinkAll() {
+        for (Node<E> x = first; x != null; ) {
+            Node<E> next = x.next;
+            x.item = null;
+            x.next = null;
+            x.prev = null;
+            x = next;
+        }
+        first = last = null;
+        size = 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("LinkedList{");
+        for (Node<E> x = first; x != null; ) {
+            sb.append(" ").append(x.item).append(",");
+            x = x.next;
+        }
+        sb.append("}");
+        return sb.toString();
     }
 }
